e756ef4d1adabf137e694d1956866b5b0243feca
taylsj <taylsj@farmingdale.edu>
2022-10-29 14:38:17 -0400

    updates to profile and dataconnector classes 11/5/22

--- src/main/java/com/example/seniorproject1/DatabaseConnection.java
+++ src/main/java/com/example/seniorproject1/DatabaseConnection.java
@@ -9,11 +9,15 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseConnection {
-    public Connection databaseLink;
     private Connection conn;
+    private PreparedStatement preparedStatement;
 
     public Connection getConnection() {
         //String databaseName = "SeniorProject";
@@ -41,6 +45,109 @@ public class DatabaseConnection {
 */
     }
 
+    public String getAboutMe(String profileName) {
+        String tableName = "ProfileInfo";
+        ResultSet result = null;
+        String returnStr = "";
+        try {
+            Statement stmt = conn.createStatement();
+            result = stmt.executeQuery("SELECT aboutMe FROM " + tableName + " WHERE profileName = \'" + profileName + "\'");
+            while (result.next()) {
+                returnStr = result.getString("aboutMe");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnStr;
+
+    }
+
+    //method only works for updating ProfileInfo table
+    public boolean updateColumn(String t, String userName, String newStr, String col) {
+        //UPDATE ProfileInfo  SET aboutMe = 'test123' WHERE profileName = 'AB' ;
+        try {
+            // String sql = "UPDATE" + t + " SET " + col + " =\'" + newStr + "\' WHERE profileName =\'" + userName + "\'";
+            String sql = "UPDATE ProfileInfo  SET aboutMe = 'test123' WHERE profileName = 'AB' ";
+
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Successfully Updated");
+            return true;
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return false;
+    }
+
+    public boolean updateAboutMe(String userName, String newStr) {
+        try {
+            // String sql = "UPDATE" + t + " SET " + col + " =\'" + newStr + "\' WHERE profileName =\'" + userName + "\'";
+            String sql = "UPDATE ProfileInfo SET aboutMe = \'" + newStr + "\' WHERE profileName = \'" + userName + "\'";
+
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Successfully Updated");
+            return true;
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return false;
+    }
+
+
+
+
+    public int getUserID(String un) {
+        int ans = -1;
+        ResultSet result = null;
+        try {
+            Statement stmt = conn.createStatement();
+            result = stmt.executeQuery("SELECT userID FROM  UserInfo WHERE userName = \'" + un + "\'");
+            while (result.next()) {
+                ans = result.getInt("userID");
+                System.out.println("UserID = " + ans);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ans;
+    }
+
+    public void insertProfile(int userID, int gameID, int platformID, String profilePic, String aboutMe, int Rating, String profileName) {
+        //call the getConnectionDB method
+        //getConnectionDB();
+        System.out.println("insertProfile method running...");
+        String tableName = "ProfileInfo";
+        try {
+            String sql = "INSERT INTO " + tableName + " (userID, gameID, platformID, profilePic, aboutMe, Rating, profileName)  VALUES"
+                    + "(?, ?, ?, NULL, ?, ?, ?)";
+            System.out.println("Prepared Statement = " + sql);
+            preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, userID);
+            preparedStatement.setInt(2, 5);
+            preparedStatement.setInt(3, 5);
+            //preparedStatement.setString(4, );
+            preparedStatement.setString(4, aboutMe);
+            preparedStatement.setInt(5, Rating);
+            preparedStatement.setString(6, profileName);
+            int row = preparedStatement.executeUpdate();
+            System.out.println("Prepared Statement = " + preparedStatement.toString());
+            if (row > 0) {
+                System.out.println("**NEW Profile inserted into table");
+            }
+        } catch (SQLException e) {
+        }
+
+    }
+        public static void main(String [] args) {
+    DatabaseConnection dc = new DatabaseConnection();
+    dc.getConnection();
+    int x = dc.getUserID("Mike1");
+    //dc.getAboutMe(x);
+    //System.out.println(dc.getAboutMe(8));
+    dc.insertProfile(1, 1, 1," ", "abc", 1, "hey");
+
+    }
 
 
 }
--- src/main/java/com/example/seniorproject1/Main.java
+++ src/main/java/com/example/seniorproject1/Main.java
@@ -5,10 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login1.fxml"));
@@ -21,5 +24,29 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
+    }*/
+    private static Scene scene;
+    public static Stage mainStage;
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("login1"), 502, 593);
+        stage.setScene(scene);
+        stage.show();
+        mainStage = stage;
     }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
 }
